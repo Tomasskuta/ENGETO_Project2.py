453cc704ca63514674a536dfdb41d36021aef442
Tomasskuta <tomasskuta@seznam.cz>
2024-06-11 11:27:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 play_field = [[" " for _ in range(3)] for _ in range(3)]
 
-def playgame():
+while True:
 
-    playerX = input("Where do you want to place your X (row,column)? ")
+    playerX = input("Where do you want to place your X (row column)? ")
     playerXr = int(playerX[0]) - 1
     playerXc = int(playerX[2]) - 1
     print(playerXr)
@@ -10,25 +10,33 @@ def playgame():
 
     play_field[playerXr][playerXc] = "X"
     print("\n".join(str(play_field[i]) for i in range(3)))
+    while True:
+
+        try:
+            playerY = input("Where do you want to place your Y (row column)? ")
 
 
-    playerY = input("Where do you want to place your Y (row,column)? ")
-    playerYr = int(playerY[0]) - 1
-    playerYc = int(playerY[2]) - 1
-    print(playerYr)
-    print(playerYc)
+            if not ((1 <= int(playerY[0]) <= 3 or 1 <= int(playerY[1]) <= 3)):
+                break
+            playerYr = int(playerY[0]) - 1
+            playerYc = int(playerY[2]) - 1
+            break
+
+        except ValueError:
+            print("Wrong input, try again")
+            continue
+        except InputOutsideRange:
+            print("Numbers outside of the range 1-3")
+             
+
+        playerYr = int(playerY[0]) - 1
+        playerYc = int(playerY[2]) - 1
+        print(playerYr)
+        print(playerYc)
 
     play_field[playerYr][playerYc] = "Y"
     print("\n".join(str(play_field[i]) for i in range(3)))
 
-    checkgame()
-
-def checkgame():
-    check_question = input("Wanna play again (Y/N): ")
-    while check_question == "Y":
-        playgame()
-
-playgame()
 
 
 
