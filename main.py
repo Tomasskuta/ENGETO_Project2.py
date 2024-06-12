977d1290e53cb0ca81c34abf2cf8dfb1ff5ff764
Tomasskuta <tomasskuta@seznam.cz>
2024-06-12 15:14:21 +0200

    Step by step
    Added whole header with also clearing of console/terminal to have clear vision of play field. Then finished exceptions more or less. Next step was to change players after each play

--- main.py
+++ main.py
@@ -1,41 +1,70 @@
-play_field = [[" " for _ in range(3)] for _ in range(3)]
+import os
 
-while True:
+def print_field(field):
 
-    playerX = input("Where do you want to place your X (row column)? ")
-    playerXr = int(playerX[0]) - 1
-    playerXc = int(playerX[2]) - 1
-    print(playerXr)
-    print(playerXc)
+    os.system("cls")
+    print("Welcome to Tic Tac Toe")
+    print("============================================")
+    print("GAME RULES:")
+    print("Each player can place one mark (or stone)")
+    print("per turn on the 3x3 grid. The WINNER is")
+    print("who succeeds in placing three of their")
+    print("marks in a:")
+    print("* horizontal,")
+    print("* vertical or")
+    print("* diagonal row")
+    print("============================================")
+    print("Let's start the game")
+    print("--------------------------------------------")
+    print("o---o---o---o")
+    for row in field:
+        print("| {} | {} | {} |".format(*row))
+        print("o---o---o---o")
+    print("============================================")
 
-    play_field[playerXr][playerXc] = "X"
-    print("\n".join(str(play_field[i]) for i in range(3)))
+def play(field,player_tick):
+
     while True:
 
         try:
-            playerY = input("Where do you want to place your Y (row column)? ")
+            playerYin = input(f"Player {player_tick} | Please enter your move numbers 'row column': ")
 
+            player = playerYin.split()
 
-            if not ((1 <= int(playerY[0]) <= 3 or 1 <= int(playerY[1]) <= 3)):
-                break
-            playerYr = int(playerY[0]) - 1
-            playerYc = int(playerY[2]) - 1
-            break
+            if len(player) != 2:
+                raise ValueError("Input must be in format: row column")
+            
+            if not (player[0].isdigit() and player[1].isdigit()):
+                raise ValueError("Both row and column must be digits")
 
-        except ValueError:
-            print("Wrong input, try again")
+            if not ((1 <= int(player[0]) <= 3 and 1 <= int(player[1]) <= 3)):
+                raise ValueError("Input is not between 1-3")
+
+        except ValueError as exception:
+            print(exception)
             continue
-        except InputOutsideRange:
-            print("Numbers outside of the range 1-3")
-             
 
-        playerYr = int(playerY[0]) - 1
-        playerYc = int(playerY[2]) - 1
-        print(playerYr)
-        print(playerYc)
+        playerYr = int(player[0]) - 1
+        playerYc = int(player[1]) - 1
 
-    play_field[playerYr][playerYc] = "Y"
-    print("\n".join(str(play_field[i]) for i in range(3)))
+        field[playerYr][playerYc] = player_tick
+        if player_tick != "X":
+            player_tick = "X"
+        else:
+            player_tick = "O"
+   
+        print_field(field)
+
+def main():
+    field = [[" " for _ in range(3)] for _ in range(3)]
+    player_tick = "O"
+    print_field(field)
+    play(field,player_tick)
+
+if __name__ == "__main__":
+    main()
+
+        
 
 
 
