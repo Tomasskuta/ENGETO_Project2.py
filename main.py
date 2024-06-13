71f6495184cb05d247184c58754afecdae510e91
Tomasskuta <tomasskuta@seznam.cz>
2024-06-13 15:08:06 +0200

    whole functionality update
    now missing only code for checking the winner. Rest is basically done - but with little bit confusion while using functions etc - but getting into it. Just need to try few more things

--- main.py
+++ main.py
@@ -40,20 +40,50 @@ def play(field,player_tick):
             if not ((1 <= int(player[0]) <= 3 and 1 <= int(player[1]) <= 3)):
                 raise ValueError("Input is not between 1-3")
 
+            playerR = int(player[0]) - 1
+            playerC = int(player[1]) - 1
+
+            if field[playerR][playerC] == " ":
+                field[playerR][playerC] = player_tick
+            else:
+                raise ValueError("There is already something!")
+
         except ValueError as exception:
             print(exception)
             continue
 
-        playerYr = int(player[0]) - 1
-        playerYc = int(player[1]) - 1
+        winner_quest = input("Do we have winner?: ")
+        if winner_quest == "Y":
+            winner = True
+        else:
+            winner = False
 
-        field[playerYr][playerYc] = player_tick
+        if check_winner(winner):
+            print(f"Player {player_tick} won!!!!")
+            if end_game():
+                main()
+            else:
+                exit()
+
         if player_tick != "X":
             player_tick = "X"
         else:
             player_tick = "O"
-   
+
         print_field(field)
+
+def end_game():
+    play_again = input("Do you wanna play again (Y/N)?: ")
+    if play_again == "Y":
+        return True
+    else:
+        return False
+        
+def check_winner(winner):
+    if winner == True:
+        return True
+    else:
+        return False
 
 def main():
     field = [[" " for _ in range(3)] for _ in range(3)]
